e2452ffca32e8fe818a3668d120be8c4c16c2b3f
josernesto <jecm3>
2019-04-05 21:51:05 -0400

    commit compleo mvc

---
+++ MVC5/MVC5/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC5.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVC5.Models.Product> Products { get; set; }
+    }
+}
---
+++ MVC5/MVC5/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5.Models
+{
+    public enum Unitype
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+
+    }
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+       
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public Unitype Unit { get; set; }
+        public StatusType Status { get; set; }
+
+    }
+}
